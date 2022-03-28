4d411204e09966593f13bbf418a48aaa1292961f
lentyss <elenatur03@outlook.com>
2022-03-28 12:01:27 +0300

    bd v1.0

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'a really really really really long secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stud_ws.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return 1
+
+
+if __name__ == "__main__":
+    app.run()
+
