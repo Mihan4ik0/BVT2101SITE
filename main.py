1f3f3f6b3abfcf96e5bdf8b04ad8c5f1a2548204
Mihan4ik0 <miha23112003@gmail.com>
2022-03-29 11:59:04 +0300

    take all changes from others branch

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from app import create_app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = create_app()
-app.run()
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'a really really really really long secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stud_ws.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return 1
+
+
+if __name__ == "__main__":
+    app.run()
+
