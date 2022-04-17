d3ddd2a7f93686a1cfb83fa2d0c59b7e123cf169
Mihan4ik0 <miha23112003@gmail.com>
2022-04-17 20:43:55 +0300

    Small fixes again

--- app/main/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('main', __name__)  # имя схемы элементов
-
-from app.main import routes
--- app/main/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, flash, redirect, url_for, request, current_app
-from app.main import bp
-
-
-@bp.route('/', methods=['GET', 'POST'])
-def index():  # логика главной страницы
-    return render_template('news.html')
