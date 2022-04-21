9651bc0fc5f0a20ebd5ee3a2e95974160bc39075
Mihan4ik0 <miha23112003@gmail.com>
2022-04-21 18:40:49 +0300

    password reset

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
+# -*- coding: utf8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'a really really really really long secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stud_ws.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')  # адрес почты
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')  # адрес почты
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')  # пароль от почты почты
 db = SQLAlchemy(app)
+mail = Mail(app)
 migrate = Migrate(app, db)
 login = LoginManager()
 db.init_app(app)
 login.init_app(app)
 
-
-
 from routes import *
-
 
 if __name__ == "__main__":
     app.run(debug=True)
-
--- routes.py
+++ routes.py
@@ -1,7 +1,11 @@
-from main import app, db
+# -*- coding: utf8 -*-
+import random
+from main import app, db, mail
 from flask import render_template, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user
 from models import Users
+from flask_mail import Message
+
 
 
 @app.route('/')
@@ -18,8 +22,23 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/reset')
+@app.route('/reset', methods=['GET', 'POST'])
 def reset():
+    if current_user.is_authenticated:
+        return redirect(url_for('news'))
+    if request.method == 'POST' and Users.query.filter_by(email=request.form.get('login')).first() is not None:
+        email = request.form.get('login')
+        chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+        password =''
+        for i in range(16):
+            password += random.choice(chars)
+        polzovatel = Users.query.filter_by(email=request.form.get('login')).first()
+        polzovatel.password = password
+        db.session.commit()
+        msg = Message("Reset password", recipients=[email])
+        msg.body = 'Ваш новый пароль: ' + password
+        mail.send(msg)
+        return redirect('login')
     return render_template("forgot_password.html")
 
 
@@ -30,6 +49,7 @@ def update():
             if current_user.password == request.form.get('old_password') and request.form.get('n_password1') == request.form.get('n_password2'):
                 current_user.password = request.form.get('n_password1')
                 db.session.commit()
+                return redirect('news')
     return render_template("update_password.html")
 
 
